bad439e3457b658dbe988f733334b37981d4ccd3
Lorena <lorenafersilva17@gmail.com>
2023-04-13 10:47:17 -0300

    Leitor de 20 números inteiros, os pares e os impares

---
+++ exercícios5.py
@@ -0,0 +1,10 @@
+Inteiros = 0
+for i in range(0, 20):
+    Inteiros += 1
+    print (Inteiros)
+
+PAR = ["PAR", 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+print (PAR)
+
+IMPARES = ["IMPAR", 1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+print(IMPARES) 
