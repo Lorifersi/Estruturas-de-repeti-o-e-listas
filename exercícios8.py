b9d3f2c297a55bb541ebeddbc26b4dafdcdda061
Lorena <lorenafersilva17@gmail.com>
2023-04-13 11:44:22 -0300

    Leitor de Nomes e Altura na ordem inversa da escrita

---
+++ exercícios8.py
@@ -0,0 +1,7 @@
+ListaNome= ["Lorena","Maykon","Isabela","Leticia","Natalia"]
+ListaNome.reverse ()
+print(ListaNome)
+
+ListaAltura= ["1,67", "1,85", "1,58", "1,73", "1,62"]
+ListaAltura.reverse()
+print(ListaAltura)
